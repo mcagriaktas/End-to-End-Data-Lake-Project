51a992f645af35f54b31bcace13da3bdeab26008
Cagri <52080028+mcagriaktas@users.noreply.github.com>
2024-06-15 19:59:44 +0300

    Add files via upload

---
+++ mkdir_data_folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ./data
+
+mkdir -p ./data/hadoop_datanode
+mkdir -p ./data/hadoop_namenode
+mkdir -p ./data/mariadb_data
+mkdir -p ./data/mongodb_data
+mkdir -p ./data/postgresql_data
+
+
+echo "Folders created successfully."
+
